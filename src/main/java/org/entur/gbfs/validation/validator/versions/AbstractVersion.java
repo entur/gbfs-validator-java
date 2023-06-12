58cefb4dda7be04d070b43eb325ae5800e0cf43f
Tom Erik Støwer <testower@gmail.com>
2023-06-12 14:53:38 +0200

    Remove duplicated license preamble

--- src/main/java/org/entur/gbfs/validation/validator/versions/AbstractVersion.java
+++ src/main/java/org/entur/gbfs/validation/validator/versions/AbstractVersion.java
@@ -1,25 +1,5 @@
 /*
  *
- *  *
- *  *
- *  *  * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
- *  *  * the European Commission - subsequent versions of the EUPL (the "Licence");
- *  *  * You may not use this work except in compliance with the Licence.
- *  *  * You may obtain a copy of the Licence at:
- *  *  *
- *  *  *   https://joinup.ec.europa.eu/software/page/eupl
- *  *  *
- *  *  * Unless required by applicable law or agreed to in writing, software
- *  *  * distributed under the Licence is distributed on an "AS IS" basis,
- *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  * See the Licence for the specific language governing permissions and
- *  *  * limitations under the Licence.
- *  *
- *
- */
-
-/*
- *
  *
  *  * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
  *  * the European Commission - subsequent versions of the EUPL (the "Licence");
@@ -70,8 +50,6 @@ public abstract class AbstractVersion implements Version {
     private final List<String> feeds;
     private Map<String, JSONObject> schemas = new ConcurrentHashMap<>();
     private final Map<String, List<CustomRuleSchemaPatcher>> customRules;
-
-    //private final List<CustomRuleSchemaPatcher> customRules = new ArrayList<>();
 
     protected AbstractVersion(String versionString, List<String> feeds, Map<String, List<CustomRuleSchemaPatcher>> customRules) {
         this.versionString = versionString;
