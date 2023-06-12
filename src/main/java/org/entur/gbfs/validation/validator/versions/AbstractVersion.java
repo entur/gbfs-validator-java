dd223083cdab6429d20665dd4172c6f35f5b1da9
Tom Erik Støwer <testower@gmail.com>
2023-06-12 15:01:51 +0200

    Fix some smells

--- src/main/java/org/entur/gbfs/validation/validator/versions/AbstractVersion.java
+++ src/main/java/org/entur/gbfs/validation/validator/versions/AbstractVersion.java
@@ -93,7 +93,7 @@ public abstract class AbstractVersion implements Version {
     }
 
     private JSONObject getRawSchema(String feedName) {
-        schemas.computeIfAbsent(feedName, _ -> loadRawSchema(feedName));
+        schemas.computeIfAbsent(feedName, this::loadRawSchema);
         return schemas.get(feedName);
     }
 
