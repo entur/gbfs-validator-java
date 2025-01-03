67aeb3aeff9b3b892a837fce61eb83ac028fb250
Holger Bruch <hb@mfdz.de>
2025-01-03 18:10:49 +0100

    feature: check vehicle_status for valid vehicle_type_id reference

--- src/main/java/org/entur/gbfs/validation/validator/versions/Version23.java
+++ src/main/java/org/entur/gbfs/validation/validator/versions/Version23.java
@@ -25,7 +25,7 @@ import org.entur.gbfs.validation.validator.rules.NoInvalidReferenceToRegionInSta
 import org.entur.gbfs.validation.validator.rules.NoInvalidReferenceToVehicleTypesInStationStatus;
 import org.entur.gbfs.validation.validator.rules.NoMissingCurrentRangeMetersInVehicleStatusForMotorizedVehicles;
 import org.entur.gbfs.validation.validator.rules.NoMissingStoreUriInSystemInformation;
-import org.entur.gbfs.validation.validator.rules.NoMissingVehicleTypeIdInVehicleStatusWhenVehicleTypesExist;
+import org.entur.gbfs.validation.validator.rules.NoMissingOrInvalidVehicleTypeIdInVehicleStatusWhenVehicleTypesExist;
 import org.entur.gbfs.validation.validator.rules.NoMissingVehicleTypesAvailableWhenVehicleTypesExists;
 
 import java.util.Arrays;
@@ -60,7 +60,7 @@ public class Version23 extends AbstractVersion {
                     new NoMissingVehicleTypesAvailableWhenVehicleTypesExists()
             ),
             "free_bike_status", List.of(
-                    new NoMissingVehicleTypeIdInVehicleStatusWhenVehicleTypesExist("free_bike_status"),
+                    new NoMissingOrInvalidVehicleTypeIdInVehicleStatusWhenVehicleTypesExist("free_bike_status"),
                     new NoMissingCurrentRangeMetersInVehicleStatusForMotorizedVehicles("free_bike_status"),
                     new NoInvalidReferenceToPricingPlansInVehicleStatus("free_bike_status")
             ),
